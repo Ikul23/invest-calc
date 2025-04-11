a551249bea4a531bf0eca5fc469a681c057895ea
Ikul23 <ikul23@icloud.com>
2025-04-12 01:59:42 +0300

    Routes added

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '.*');
-
 Route::get('/db-config', function () {
     return [
         'env_db_host' => env('DB_HOST'),
@@ -13,3 +9,13 @@ Route::get('/db-config', function () {
         'all_config' => config('database.connections.pgsql')
     ];
 });
+
+
+Route::get('/', function () {
+    return view('app');
+});
+
+
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
