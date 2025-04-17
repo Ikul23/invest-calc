46b38dfce3a02d5034b442f2a45541ec801d6db4
Ikul23 <ikul23@icloud.com>
2025-04-17 17:34:44 +0300

    Render new deploy 2

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-set -e  # Остановить скрипт при любой ошибке
+set -e
 
 cd /var/www/html
 
-# Кэширование конфигураций Laravel
+# Удаляем .env, если он случайно был скопирован
+rm -f .env
+
+# Печатаем текущий DB_HOST, чтобы увидеть, что реально подставилось
+echo "DB_HOST from Laravel env: $DB_HOST"
+
+# Очистка кэша (на всякий случай перед миграциями)
 php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+# Кэширование снова
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Применить миграции
+# Миграции
 php artisan migrate --force
 
-# Установить права
+# Права
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
-# Запустить PHP-FPM и Nginx
+# Стартуем
 php-fpm -D
 nginx -g "daemon off;"
