098279b147450afe5dcbfab962539be1ff2d8cbe
Ikul23 <ikul23@icloud.com>
2025-04-21 20:33:45 +0300

    production build for render deploy

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     server: {
-    host: '0.0.0.0',
-    port: 5173,
-    origin: 'http://localhost:5173',
-    cors: true,
-    proxy: {
-    '/api': 'http://localhost:8000',
-  },
-},
+        host: '0.0.0.0',
+        port: 5173,
+        origin: 'http://localhost:5173',
+        cors: true,
+        proxy: {
+            '/api': 'http://localhost:8000',
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/main.jsx',
@@ -21,9 +21,8 @@ export default defineConfig({
     ],
     build: {
         outDir: 'public/build',
-    emptyOutDir: true,
-    manifest: true,
-        chunkSizeWarningLimit: 1000,
+        emptyOutDir: true,
+        manifest: true,
     },
     base: '/build/',
 });
