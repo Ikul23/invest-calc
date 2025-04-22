f452cf0a360a05660a1166bc74f9641e715ed174
Ikul23 <ikul23@icloud.com>
2025-04-22 20:20:38 +0300

    Metrics_table prod migration

---
+++ database/migrations/2025_04_22_171432_add_missing_columns_to_project_metrics.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('project_metrics', function (Blueprint $table) {
+            $table->decimal('depreciation_total')->nullable();
+            $table->decimal('ebit_total')->nullable();
+            $table->decimal('net_income_total')->nullable();
+            $table->decimal('working_capital_total')->nullable();
+            $table->decimal('initial_investment')->nullable();
+            $table->decimal('average_tax_rate')->nullable();
+            $table->decimal('cumulative_cashflow')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('project_metrics', function (Blueprint $table) {
+            $table->dropColumn([
+                'depreciation_total',
+                'ebit_total',
+                'net_income_total',
+                'working_capital_total',
+                'initial_investment',
+                'average_tax_rate',
+                'cumulative_cashflow',
+            ]);
+        });
+    }
+}
